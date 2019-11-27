d050d5502dccecc88f5b79255518ab6690c0b6bf
KoenVermeulen91 <vermeulen.koen@me.com>
2019-11-27 08:27:55 +0100

    2nd commit: halfway

--- Draft_script.R
+++ Draft_script.R
@@ -8,10 +8,12 @@ library(dplyr)
 library(dplyr)
 
 # If loop (or reading zip)
-activity <- read.csv("activity.csv")
+Activity <- read.csv(unzip("activity.zip"))
 
 # Checking data
 glimpse(activity)
 summary(activity)
 plot(activity$steps)
 hist(activity$steps, breaks = 100)
+
+ActivityImputed$Date <- as.Date(ActivityImputed$date)
